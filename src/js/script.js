a7ebef53230148ac28942309a267ff03f8aa5407
RLVictor <vityad888@yandex.ru>
2022-12-22 19:09:54 +0300

    22.12.22

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
   $(".slider").slick({
-    slidesToShow: 3,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    arrows: false,
   });
 });
