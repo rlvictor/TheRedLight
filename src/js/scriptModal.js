95df5d10a71aa359e48c8dd74f7a5c8741639f46
RLVictor <vityad888@yandex.ru>
2023-01-13 19:46:01 +0300

    13.01.23

--- src/js/scriptModal.js
+++ src/js/scriptModal.js
@@ -4,5 +4,56 @@ $(document).ready(function () {
   });
   $(".modal__close").on("click", function () {
     $(".overlay, .modal").fadeOut("slow");
+    window.location.reload();
+  });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите не менее {0} символов!"),
+        },
+        phone: "Пожалуйста, введите свой номер телефона ",
+        email: {
+          required: "Пожалуйста, назовите свою почту",
+          email: "Неправильно введен адрес почты",
+        },
+        textarea: {
+          required: "Напишите пожелание или свой вопрос",
+        },
+      },
+    });
+  }
+  validateForms(".feed-form");
+
+  // Отправка на почту
+  $("form").submit(function (e) {
+    e.preventDefault();
+    if (!$(this).valid()) {
+      return;
+    }
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $(".modal, .feed-form").fadeOut("slow");
+      $(".modal__feed").fadeIn("slow");
+      $("form").trigger("reset");
+    });
+    return false;
   });
 });
