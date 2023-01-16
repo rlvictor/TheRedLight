478a5a2a4c04513838e0939b92cbaf3ba695d12a
RLVictor <vityad888@yandex.ru>
2023-01-16 22:45:53 +0300

    16.01.23

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,3 @@
-$(document).ready(function () {
-  $(".slider").slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    arrows: false,
-  });
-});
-
 window.addEventListener("DOMContentLoaded", () => {
   const menu = document.querySelector(".menu"),
     menuList = document.querySelectorAll(".menu"),
